ef18ef6ee410257a7005ac11911ba0796b27196d
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-08-23 21:26:49 +0200

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,5 +5,5 @@ for num in range(1, 100):
     if num % 5 == 0:
         print("Buzz", end='')
     if num % 3 and num % 5:
-        print("FizzBuzz", end='')
+        print("{:d}".format(num), end='')
     print(end=' ')
