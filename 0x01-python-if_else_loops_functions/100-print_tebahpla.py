3ec21261e4f5dba47de50d393d6acb0318ad1b4f
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-08-23 22:46:48 +0200

    print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((char - (ord('a') - ord('A'))) if char % 2 else char), end='')
+    print("{:c}".format((char - (ord('a') - ord('A')))
+                        if char % 2 else char), end='')
