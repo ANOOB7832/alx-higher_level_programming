241e64b95817c90ec8b3265004fe79e7064af0b3
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-11-30 12:39:06 +0200

    counting arguments when running function

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const procs = [];
 let counts = 0;
 
 argv.forEach((val, index) => {
-    counts += 1});
-    counts = counts - 2;
-  if (counts > 1) {
-    console.log('Arguments found')
-  } else if (counts > 0) {
-    console.log('Argument found')
-  } else if (counts === 0) {
-    console.log('No argument')
-};
+  counts += 1;
+});
+counts = counts - 2;
+if (counts > 1) {
+  console.log('Arguments found');
+} else if (counts > 0) {
+  console.log('Argument found');
+} else if (counts === 0) {
+  console.log('No argument');
+}
