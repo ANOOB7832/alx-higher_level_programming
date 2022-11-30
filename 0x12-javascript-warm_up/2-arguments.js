acfb25221575163fdbc844fd078e8acbfa9b00cc
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-11-30 12:42:21 +0200

    counting arguments when running function

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,11 +5,10 @@ let counts = 0;
 argv.forEach((val, index) => {
   counts += 1;
 });
-counts = counts - 2;
-if (counts > 1) {
+if (counts > 3) {
   console.log('Arguments found');
-} else if (counts > 0) {
+} else if (counts > 2) {
   console.log('Argument found');
-} else if (counts === 0) {
+} else if (counts === 2) {
   console.log('No argument');
 }
