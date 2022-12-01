4606c8e0325f7a81ef2ac217da16dae7b6abf7a1
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-12-01 14:12:35 +0200

    printing 'C is fun' an argument amount of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,3 +1,15 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+const procs = [];
+let argNum = [];
+let i = 1;
 
+argv.forEach((val) => {
+  procs.push(val);
+  argNum = Number(procs[procs.length - 1]);
+});
 
+do {
+  i = i + 1;
+  console.log('C is fun');
+} while (argNum >= i);
