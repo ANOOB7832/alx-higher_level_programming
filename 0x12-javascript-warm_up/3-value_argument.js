fc5cebf8080d9cec30e2c1d5680a7ee706ed51ac
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-12-01 11:47:08 +0200

    returns argument entered, no 'No argument'

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-let procs = [];
-let counts = [];
-let first_val = '';
+const procs = [];
+const counts = [];
+let firstVal = [];
 
 argv.forEach((val, index) => {
-    procs.push(val);
-    counts.push(index);
-    const rem_vals = [process.execPath, __filename];
-    return (first_val = procs.pop(rem_vals));
-    console.log(process.execArgv);
+  procs.push(val);
+  counts.push(index);
+  const remVals = [process.execPath, __filename];
+  return (firstVal = procs.slice(2));
 });
 
-if (first_val == __filename) {
-    console.log('No argument')
+if (firstVal === __filename) {
+  console.log('No argument');
 } else {
-    console.log(first_val)
+  console.log(firstVal[0]);
 }
