94923fb5c498aec52f0c7d25d0d1bc5034ef29a6
A NOOB <anoob@A_NOOB_PC.localdomain>
2022-12-02 11:20:23 +0200

    My first commit

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This project is practice for importing modules
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """Addition Function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        Result of a + b
+    """
+    return a + b
